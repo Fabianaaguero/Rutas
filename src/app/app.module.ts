ccadd01a2822e731f3685bd9cf64f1fe5cb0a844
Fabianaaguero <fabiana.aguero22@gmail.com>
2023-09-21 09:54:09 -0300

    Creación componentes vacíos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { TablaComponent } from './componentes/tabla/tabla.component';
+import { NavComponent } from './componentes/nav/nav.component';
+import { ImagenesComponent } from './componentes/imagenes/imagenes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TablaComponent,
+    NavComponent,
+    ImagenesComponent
   ],
   imports: [
     BrowserModule,
